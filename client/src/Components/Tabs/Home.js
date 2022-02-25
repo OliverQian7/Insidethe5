2d3fd05d438b5995aa6d790f15a3a914a0d94d20
Constantine <684107@sylvaniastudents.org>
2022-02-25 14:22:53 -0500

    chicken in the project

--- client/src/Components/Tabs/Home.js
+++ client/src/Components/Tabs/Home.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div>
             <h1>Welcome to Inside the Five</h1>
-            <img src="Chicken.PNG"></img>
+            <img src="Images/Chicken.PNG"></img>
         </div>
         
     )
