a4329ccc0f86eee5b17e7fbfb017d6f688d0b32f
Constantine <684107@sylvaniastudents.org>
2022-03-04 14:22:25 -0500

    menu stuff

--- client/src/Components/Tabs/Menu.js
+++ client/src/Components/Tabs/Menu.js
@@ -1,9 +1,13 @@
 function Menu() {
     return (
         <div>
-            <h1>Our Menu</h1>
-            <subtitle1 id="menubody">
+            <h1 id = "MenuTop">Our Menu</h1>
+            <h3 id = "menu5">$5 Apps</h3>
+            <h3 id = "menu10">$10 Menu</h3>
+            <h3 id = "menu15">$15 Menu</h3>
 
+            <subtitle1 id="menubody">
+            Menu
             </subtitle1>
         </div>
     )
