7fcb422d224e77119196c870d2087f1b1bdb0b23
OliverQian7 <71441588+OliverQian7@users.noreply.github.com>
2022-03-07 14:23:02 -0500

    new tabs + navbar stuff

--- client/src/Components/Tabmanager.js
+++ client/src/Components/Tabmanager.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import Home from "./Tabs/Home";
 import Menu from "./Tabs/Menu";
+import Catering from "./Tabs/Catering";
+import Contact from "./Tabs/Contact";
+import Login from "./Tabs/Login";
+import Order from "./Tabs/Order";
 
 
 function Tabmanager() {
@@ -12,12 +16,28 @@ function Tabmanager() {
     const handleMenu = () => {
         setActiveTab(<Menu />)
     }
+    const handleCatering = () => {
+        setActiveTab(<Catering />)
+    }
+    const handleContact = () => {
+        setActiveTab(<Contact />)
+    }
+    const handleLogin = () => {
+        setActiveTab(<Login />)
+    }
+    const handleOrder = () => {
+        setActiveTab(<Order />)
+    }
         return (
             <div>
                 <header>
                     <ul>
                         <li><a  onClick={handleHome}>Home</a></li>
                         <li><a  onClick={handleMenu}>Menu</a></li>
+                        <li><a  onClick={handleCatering}>Catering</a></li>
+                        <li><a  onClick={handleContact}>Contact Us</a></li>
+                        <li><a  onClick={handleLogin}>Register/Login</a></li>
+                        <li><a  onClick={handleOrder}>Order Online</a></li>
                     </ul>
                 </header>
                 <body>
