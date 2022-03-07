b55a94f00ca1036202346220db20ef7926f3595e
Constantine <684107@sylvaniastudents.org>
2022-03-07 14:21:26 -0500

    full menu html

--- client/src/Components/Tabs/Menu.js
+++ client/src/Components/Tabs/Menu.js
@@ -8,7 +8,32 @@ function Menu() {
                     <th>$10 Menu</th>
                     <th>$15 Menu</th>
                 </tr>
+                <tr>
+                    <th>Loaded Beer Queso: CANTON Cheese dip loaded with corn, Black beans, pico - served with your choice of chips or flatbread</th>
+                    <th>Pub Burger: 8oz burger with your choice of cheese, lettuce, onion, tomato, and pickles (Make it vegetarian and sub beyond burger +2)</th>
+                    <th>Beef stroganoff: Our take on a midwest favorite. made-to-order with beef braised brisket, mushrooms, a rich beef jus and smoked paprika soura cream all tossed with egg noodle.</th>
+                </tr>
+                <tr>
+                    <th>Cauliflower bites: Flash fried and tosed in your choice of sauce or seasoning - served with ranch or blue cheese.</th>
+                    <th>The IT5 burger: 80z Burger with boursin, smoked gouda + roasted garlic onion jam.</th>
+                    <th>ITF Meatloaf: Traditional meatloaf finished on the chargrill served atop creamy mashed potatoes with a chipotle bourbon glaze and onion tangles.</th>
+                </tr>
+                <tr>
+                    <th>Bavarian Pretzel: served with beer cheese and whole grain mustard</th>
+                    <th>The Medi Wrap: Romaine and kale toosed in our Kalamaa Vinaigrette, then wrapped in a flour tortilla with hummus, fattoush, and feta.</th>
+                    <th>Chicken + Waffles: Buttermilk fried chicken topped with cheda between two pearl sugar waffles, served with Demios syurp and whole grain honey mustard.</th>
+                </tr>
+                <tr>
+                    <th>Street Corn Dip: Roasted sweet corn in a creamy dip topped with cotija cheese and cilantro - served with your choice of chips or flatbread</th>
+                    <th>The Nashville: Buttermilk fried chicken dipped in our Nashville sauce with pickles on a brioche bun. Spicy + Delicious</th>
+                    <th>The Cubano: Slow roasted pork carnitas and smoked ham with a mustard bbq sauce cheddar cheese and pickles.</th>
+                </tr>
+                <tr>
+                    <th>Jalapeno Bacon Balls:Fresh jalapeno, smoked bacon, herbed cream cheese and gouda, fried and served with lime cilantro aioli</th>
+                    <th>Mac N Cheese Bowl: Cavatappi pasta and crispy bacon tossed in our made-to-order Ale Cheddar cheese sauce</th>
+                </tr>
             </table>
+            
 
 
             <subtitle1 id="menubody">
