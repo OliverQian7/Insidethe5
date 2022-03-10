6b8e5ef38818352f6f1c3e088b3ff2706e9b5942
Constantine <684107@sylvaniastudents.org>
2022-03-10 13:48:34 -0500

    Revert "Merge branch 'Const' into oliver"
    This reverts commit a5dda9ee965925610d936c9242b7e0a13e454650, reversing
    changes made to 097114f5f9082fc857d93446f058541faaea1e9b.

--- client/src/Components/Tabs/Catering.js
+++ client/src/Components/Tabs/Catering.js
@@ -3,35 +3,7 @@ import React from 'react'
 function Catering() {
     return (
         <div>
-            <h1>Private Events</h1>
-            <subtitle1 id="caterwarning">
-                We currently do not take eservations other than private vents with a room rental fee and minimum for an quesitons regarding large parties please email us at constact@insidethefive.com.
-            </subtitle1>
-            <br></br>
-            <subtitle1 id="eventbody">
-                Our private rooms are designed to fit all special events including birthday parties, cocktail hour, business meetings, wedding receptions reunions and more. Please click the button below to fill out an inquiry form on our Event Database.
-            </subtitle1>
-            <br></br>
-            <subtitle1 id="eventfee1">
-                Monday-Thursday(all day) and Friday until 4pm.
-            </subtitle1>
-            <br></br>
-            <subtitle1 id="eventfee2">
-                $250 room fee rental
-                <br></br>
-                $500 food and beverage minimum
-            </subtitle1>
-            <br></br>
-            <subtitle1 id="eventfee3">
-                Friday after 4pm nd saturday-sunday(all day)
-            </subtitle1>
-            <br></br>
-            <subtitle1 id="eventfee4">
-                $400 room rental fee
-                <br>
-                </br>
-                Sylvania $1500 food and beverage minimum
-            </subtitle1>
+            
         </div>
     )
 }
